4564ac278dcf9c5169163c11c3c027bcc0e688bf
Landon Bouma <landon.bouma@gmail.com>
2015-09-23 17:11:13 -0500

    Rename files, modules and variables per new project name.

--- jsonishtest.py
+++ jsonishtest.py
@@ -22,8 +22,8 @@
 
 import unittest
 
-import cjson
-_exception = cjson.DecodeError
+import cjsonish
+_exception = cjsonish.DecodeError
 
 # The object tests should be order-independent. They're not.
 # i.e. they should test for existence of keys and values
@@ -34,204 +34,204 @@ def _removeWhitespace(str):
 
 class JsonTest(unittest.TestCase):
     def testReadEmptyObject(self):
-        obj = cjson.decode("{}")
+        obj = cjsonish.decode("{}")
         self.assertEqual({}, obj)
 
     def testWriteEmptyObject(self):
-        s = cjson.encode({})
+        s = cjsonish.encode({})
         self.assertEqual("{}", _removeWhitespace(s))
 
     def testReadStringValue(self):
-        obj = cjson.decode('{ "name" : "Patrick" }')
+        obj = cjsonish.decode('{ "name" : "Patrick" }')
         self.assertEqual({ "name" : "Patrick" }, obj)
 
     def testReadEscapedQuotationMark(self):
-        obj = cjson.decode(r'"\""')
+        obj = cjsonish.decode(r'"\""')
         self.assertEqual(r'"', obj)
 
 #    def testReadEscapedSolidus(self):
-#        obj = cjson.decode(r'"\/"')
+#        obj = cjsonish.decode(r'"\/"')
 #        self.assertEqual(r'/', obj)
 
     def testReadEscapedReverseSolidus(self):
-        obj = cjson.decode(r'"\\"')
+        obj = cjsonish.decode(r'"\\"')
         self.assertEqual("\\", obj)
 
     def testReadEscapedBackspace(self):
-        obj = cjson.decode(r'"\b"')
+        obj = cjsonish.decode(r'"\b"')
         self.assertEqual("\b", obj)
 
     def testReadEscapedFormfeed(self):
-        obj = cjson.decode(r'"\f"')
+        obj = cjsonish.decode(r'"\f"')
         self.assertEqual("\f", obj)
 
     def testReadEscapedNewline(self):
-        obj = cjson.decode(r'"\n"')
+        obj = cjsonish.decode(r'"\n"')
         self.assertEqual("\n", obj)
 
     def testReadEscapedCarriageReturn(self):
-        obj = cjson.decode(r'"\r"')
+        obj = cjsonish.decode(r'"\r"')
         self.assertEqual("\r", obj)
 
     def testReadEscapedHorizontalTab(self):
-        obj = cjson.decode(r'"\t"')
+        obj = cjsonish.decode(r'"\t"')
         self.assertEqual("\t", obj)
 
     def testReadEscapedHexCharacter(self):
-        obj = cjson.decode(r'"\u000A"')
+        obj = cjsonish.decode(r'"\u000A"')
         self.assertEqual("\n", obj)
-        obj = cjson.decode(r'"\u1001"')
+        obj = cjsonish.decode(r'"\u1001"')
         self.assertEqual(u'\u1001', obj)
 
     def testWriteEscapedQuotationMark(self):
-        s = cjson.encode(r'"')
+        s = cjsonish.encode(r'"')
         self.assertEqual(r'"\""', _removeWhitespace(s))
 
     def testWriteEscapedSolidus(self):
-        s = cjson.encode(r'/')
+        s = cjsonish.encode(r'/')
         #self.assertEqual(r'"\/"', _removeWhitespace(s))
         self.assertEqual('"/"', _removeWhitespace(s))
 
     def testWriteNonEscapedSolidus(self):
-        s = cjson.encode(r'/')
+        s = cjsonish.encode(r'/')
         self.assertEqual(r'"/"', _removeWhitespace(s))
 
     def testWriteEscapedReverseSolidus(self):
-        s = cjson.encode("\\")
+        s = cjsonish.encode("\\")
         self.assertEqual(r'"\\"', _removeWhitespace(s))
 
     def testWriteEscapedBackspace(self):
-        s = cjson.encode("\b")
+        s = cjsonish.encode("\b")
         self.assertEqual(r'"\b"', _removeWhitespace(s))
 
     def testWriteEscapedFormfeed(self):
-        s = cjson.encode("\f")
+        s = cjsonish.encode("\f")
         self.assertEqual(r'"\f"', _removeWhitespace(s))
 
     def testWriteEscapedNewline(self):
-        s = cjson.encode("\n")
+        s = cjsonish.encode("\n")
         self.assertEqual(r'"\n"', _removeWhitespace(s))
 
     def testWriteEscapedCarriageReturn(self):
-        s = cjson.encode("\r")
+        s = cjsonish.encode("\r")
         self.assertEqual(r'"\r"', _removeWhitespace(s))
 
     def testWriteEscapedHorizontalTab(self):
-        s = cjson.encode("\t")
+        s = cjsonish.encode("\t")
         self.assertEqual(r'"\t"', _removeWhitespace(s))
 
     def testWriteEscapedHexCharacter(self):
-        s = cjson.encode(u'\u1001')
+        s = cjsonish.encode(u'\u1001')
         self.assertEqual(r'"\u1001"', _removeWhitespace(s))
 
     def testReadBadEscapedHexCharacter(self):
         self.assertRaises(_exception, self.doReadBadEscapedHexCharacter)
 
     def doReadBadEscapedHexCharacter(self):
-        cjson.decode('"\u10K5"')
+        cjsonish.decode('"\u10K5"')
 
     def testReadBadObjectKey(self):
         self.assertRaises(_exception, self.doReadBadObjectKey)
 
     def doReadBadObjectKey(self):
-        cjson.decode('{ 44 : "age" }')
+        cjsonish.decode('{ 44 : "age" }')
 
     def testReadBadArray(self):
         self.assertRaises(_exception, self.doReadBadArray)
 
     def doReadBadArray(self):
-        cjson.decode('[1,2,3,,]')
+        cjsonish.decode('[1,2,3,,]')
         
     def testReadBadObjectSyntax(self):
         self.assertRaises(_exception, self.doReadBadObjectSyntax)
 
     def doReadBadObjectSyntax(self):
-        cjson.decode('{"age", 44}')
+        cjsonish.decode('{"age", 44}')
 
     def testWriteStringValue(self):
-        s = cjson.encode({ "name" : "Patrick" })
+        s = cjsonish.encode({ "name" : "Patrick" })
         self.assertEqual('{"name":"Patrick"}', _removeWhitespace(s))
 
     def testReadIntegerValue(self):
-        obj = cjson.decode('{ "age" : 44 }')
+        obj = cjsonish.decode('{ "age" : 44 }')
         self.assertEqual({ "age" : 44 }, obj)
 
     def testReadNegativeIntegerValue(self):
-        obj = cjson.decode('{ "key" : -44 }')
+        obj = cjsonish.decode('{ "key" : -44 }')
         self.assertEqual({ "key" : -44 }, obj)
         
     def testReadFloatValue(self):
-        obj = cjson.decode('{ "age" : 44.5 }')
+        obj = cjsonish.decode('{ "age" : 44.5 }')
         self.assertEqual({ "age" : 44.5 }, obj)
 
     def testReadNegativeFloatValue(self):
-        obj = cjson.decode(' { "key" : -44.5 } ')
+        obj = cjsonish.decode(' { "key" : -44.5 } ')
         self.assertEqual({ "key" : -44.5 }, obj)
 
     def testReadBadNumber(self):
         self.assertRaises(_exception, self.doReadBadNumber)
 
     def doReadBadNumber(self):
-        cjson.decode('-44.4.4')
+        cjsonish.decode('-44.4.4')
 
     def testReadSmallObject(self):
-        obj = cjson.decode('{ "name" : "Patrick", "age":44} ')
+        obj = cjsonish.decode('{ "name" : "Patrick", "age":44} ')
         self.assertEqual({ "age" : 44, "name" : "Patrick" }, obj)        
 
     def testReadEmptyArray(self):
-        obj = cjson.decode('[]')
+        obj = cjsonish.decode('[]')
         self.assertEqual([], obj)
 
     def testWriteEmptyArray(self):
-        self.assertEqual("[]", _removeWhitespace(cjson.encode([])))
+        self.assertEqual("[]", _removeWhitespace(cjsonish.encode([])))
 
     def testReadSmallArray(self):
-        obj = cjson.decode(' [ "a" , "b", "c" ] ')
+        obj = cjsonish.decode(' [ "a" , "b", "c" ] ')
         self.assertEqual(["a", "b", "c"], obj)
 
     def testWriteSmallArray(self):
-        self.assertEqual('[1,2,3,4]', _removeWhitespace(cjson.encode([1, 2, 3, 4])))
+        self.assertEqual('[1,2,3,4]', _removeWhitespace(cjsonish.encode([1, 2, 3, 4])))
 
     def testWriteSmallObject(self):
-        s = cjson.encode({ "name" : "Patrick", "age": 44 })
+        s = cjsonish.encode({ "name" : "Patrick", "age": 44 })
         self.assertEqual('{"age":44,"name":"Patrick"}', _removeWhitespace(s))
 
     def testWriteFloat(self):
         n = 3.44556677
-        self.assertEqual(repr(n), _removeWhitespace(cjson.encode(n)))
+        self.assertEqual(repr(n), _removeWhitespace(cjsonish.encode(n)))
 
     def testReadTrue(self):
-        self.assertEqual(True, cjson.decode("true"))
+        self.assertEqual(True, cjsonish.decode("true"))
 
     def testReadFalse(self):
-        self.assertEqual(False, cjson.decode("false"))
+        self.assertEqual(False, cjsonish.decode("false"))
 
     def testReadNull(self):
-        self.assertEqual(None, cjson.decode("null"))
+        self.assertEqual(None, cjsonish.decode("null"))
 
     def testWriteTrue(self):
-        self.assertEqual("true", _removeWhitespace(cjson.encode(True)))
+        self.assertEqual("true", _removeWhitespace(cjsonish.encode(True)))
 
     def testWriteFalse(self):
-        self.assertEqual("false", _removeWhitespace(cjson.encode(False)))
+        self.assertEqual("false", _removeWhitespace(cjsonish.encode(False)))
 
     def testWriteNull(self):
-        self.assertEqual("null", _removeWhitespace(cjson.encode(None)))
+        self.assertEqual("null", _removeWhitespace(cjsonish.encode(None)))
 
     def testReadArrayOfSymbols(self):
-        self.assertEqual([True, False, None], cjson.decode(" [ true, false,null] "))
+        self.assertEqual([True, False, None], cjsonish.decode(" [ true, false,null] "))
 
     def testWriteArrayOfSymbolsFromList(self):
-        self.assertEqual("[true,false,null]", _removeWhitespace(cjson.encode([True, False, None])))
+        self.assertEqual("[true,false,null]", _removeWhitespace(cjsonish.encode([True, False, None])))
 
     def testWriteArrayOfSymbolsFromTuple(self):
-        self.assertEqual("[true,false,null]", _removeWhitespace(cjson.encode((True, False, None))))
+        self.assertEqual("[true,false,null]", _removeWhitespace(cjsonish.encode((True, False, None))))
 
     def testReadComplexObject(self):
         src = '''
     { "name": "Patrick", "age" : 44, "Employed?" : true, "Female?" : false, "grandchildren":null }
 '''
-        obj = cjson.decode(src)
+        obj = cjsonish.decode(src)
         self.assertEqual({"name":"Patrick","age":44,"Employed?":True,"Female?":False,"grandchildren":None}, obj)
 
     def testReadLongArray(self):
@@ -243,7 +243,7 @@ class JsonTest(unittest.TestCase):
     2,
     [3, 4, 5]]
 '''
-        obj = cjson.decode(src)
+        obj = cjsonish.decode(src)
         self.assertEqual(["used","abused","confused", True, False, None,
                           1,2,[3,4,5]], obj)
 
@@ -251,7 +251,7 @@ class JsonTest(unittest.TestCase):
         self.assertRaises(_exception, self.doReadIncompleteArray)
 
     def doReadIncompleteArray(self):
-        cjson.decode('[')
+        cjsonish.decode('[')
 
     def testReadComplexArray(self):
         src = '''
@@ -267,7 +267,7 @@ class JsonTest(unittest.TestCase):
     [3, 4, 5]
 ]
 '''
-        obj = cjson.decode(src)
+        obj = cjsonish.decode(src)
         self.assertEqual([{"name":"Patrick","age":44,"Employed?":True,"Female?":False,"grandchildren":None},
                           "used","abused","confused",
                           1,2,[3,4,5]], obj)
@@ -277,55 +277,55 @@ class JsonTest(unittest.TestCase):
                "used","abused","confused",
                1,2,[3,4,5]]
         self.assertEqual('[{"Female?":false,"age":44,"name":"Patrick","grandchildren":null,"Employed?":true},"used","abused","confused",1,2,[3,4,5]]',
-                         _removeWhitespace(cjson.encode(obj)))
+                         _removeWhitespace(cjsonish.encode(obj)))
 
 
     def testReadWriteCopies(self):
         orig_obj = {'a':' " '}
-        json_str = cjson.encode(orig_obj)
-        copy_obj = cjson.decode(json_str)
+        json_str = cjsonish.encode(orig_obj)
+        copy_obj = cjsonish.decode(json_str)
         self.assertEqual(orig_obj, copy_obj)
         self.assertEqual(True, orig_obj == copy_obj)
         self.assertEqual(False, orig_obj is copy_obj)
 
     def testStringEncoding(self):
-        s = cjson.encode([1, 2, 3])
+        s = cjsonish.encode([1, 2, 3])
         self.assertEqual(unicode("[1,2,3]", "utf-8"), _removeWhitespace(s))
 
     def testReadEmptyObjectAtEndOfArray(self):
         self.assertEqual(["a","b","c",{}],
-                         cjson.decode('["a","b","c",{}]'))
+                         cjsonish.decode('["a","b","c",{}]'))
 
     def testReadEmptyObjectMidArray(self):
         self.assertEqual(["a","b",{},"c"],
-                         cjson.decode('["a","b",{},"c"]'))
+                         cjsonish.decode('["a","b",{},"c"]'))
 
     def testReadClosingObjectBracket(self):
-        self.assertEqual({"a":[1,2,3]}, cjson.decode('{"a":[1,2,3]}'))
+        self.assertEqual({"a":[1,2,3]}, cjsonish.decode('{"a":[1,2,3]}'))
 
     def testEmptyObjectInList(self):
-        obj = cjson.decode('[{}]')
+        obj = cjsonish.decode('[{}]')
         self.assertEqual([{}], obj)
 
     def testObjectWithEmptyList(self):
-        obj = cjson.decode('{"test": [] }')
+        obj = cjsonish.decode('{"test": [] }')
         self.assertEqual({"test":[]}, obj)
 
     def testObjectWithNonEmptyList(self):
-        obj = cjson.decode('{"test": [3, 4, 5] }')
+        obj = cjsonish.decode('{"test": [3, 4, 5] }')
         self.assertEqual({"test":[3, 4, 5]}, obj)
 
     def testWriteLong(self):
-        self.assertEqual("12345678901234567890", cjson.encode(12345678901234567890))
+        self.assertEqual("12345678901234567890", cjsonish.encode(12345678901234567890))
 
     def testWriteLongUnicode(self):
-        # This test causes a buffer overrun in cjson 1.0.5, on UCS4 builds.
+        # This test causes a buffer overrun in cjsonish 1.0.5, on UCS4 builds.
         # The string length is only resized for wide unicode characters if
         # there is less than 12 bytes of space left. Padding with
         # narrow-but-escaped characters prevents string resizing.
         # Note that u'\U0001D11E\u1234' also breaks, but sometimes goes
         # undetected.
-        s = cjson.encode(u'\U0001D11E\U0001D11E\U0001D11E\U0001D11E'
+        s = cjsonish.encode(u'\U0001D11E\U0001D11E\U0001D11E\U0001D11E'
                          u'\u1234\u1234\u1234\u1234\u1234\u1234')
         self.assertEqual(r'"\U0001d11e\U0001d11e\U0001d11e\U0001d11e'
                          r'\u1234\u1234\u1234\u1234\u1234\u1234"', s)
